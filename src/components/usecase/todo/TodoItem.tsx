7e79cc4e2fd30b86b6ae23d41b1af5afcf173663
Arunstop <arunstop@gmail.com>
2023-02-06 16:56:39 +0000

    feat : added delete func. on todo

--- src/components/usecase/todo/TodoItem.tsx
+++ src/components/usecase/todo/TodoItem.tsx
@@ -1,31 +1,31 @@
+import { Icon } from "@iconify/react";
 import { forwardRef, useCallback } from "react";
 import { TodoData } from "../../../types/types-todo";
+import { Button } from "../../commons/Button";
 
 export const TodoItem = forwardRef<
   HTMLLIElement,
   React.HTMLAttributes<HTMLLIElement> & {
     onUpdate?: (updatedTodo: TodoData) => void;
+    onDelete?:(todoId:string)=>void;
     todo: TodoData;
   }
->(({ children, onUpdate, todo, ...props }, ref) => {
+>(({ children, onUpdate,onDelete, todo, ...props }, ref) => {
   // const handleUpdateTodo = useCallback(() => {
   //   onUpdate?.({ ...todo, done: !todo.done });
   // }, [todo]);
-
+  const isDone = todo.done;
   return (
-    <li ref={ref} {...props}>
+    <li ref={ref} {...props} className="flex gap-2 sm:gap-4 flex-row">
       <a
-        onClick={() => onUpdate?.({ ...todo, done: !todo.done })}
-        className="flex gap-2 sm:gap-4"
+        className={`flex flex-1 ${isDone ? "line-through opacity-50" : ""}`}
+        onClick={() => onUpdate?.({ ...todo, done: !isDone })}
       >
-        <input
-          type="checkbox"
-          checked={!!todo.done}
-          readOnly
-          className="checkbox"
-        />
         <span>{todo.title}</span>
       </a>
+      <span className={`flex !rounded-full opacity-50 hover:opacity-100`} onClick={()=>onDelete(todo.id)}>
+        <Icon icon="mdi:close-thick" />
+      </span>
     </li>
   );
 });
